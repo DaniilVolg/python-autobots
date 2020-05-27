bdac5e2d601564b4ac57664b5f013e0e8ce8b0bc
DaniilVolg <59584072+DaniilVolg@users.noreply.github.com>
2020-05-27 20:42:54 +0300

    Update cert test_challenge6.py

--- cert test_challenge6.py
+++ cert test_challenge6.py
@@ -1,4 +1,13 @@
-def test_ch61(py):
+
+from selenium.webdriver.common.keys import Keys
+
+from copart_com.pages.home import HomePage
+
+
+
+
+def test_challenge6(py):
+    
     py.visit('https://www.copart.com/')
     py.get("#input-search", timeout=10).type("Nissan", Keys.ENTER)
     py.get("input[type='search']").type("SKYLINE").click()
@@ -7,4 +16,14 @@ def test_ch61(py):
     except:
         "Could not find element with the text ``SKYLINE22``"
 
+    py.screenshot('../screenshots/test_challenge6_model_not_found.png')
+    
+    
+    def test_challenge6_POM(py):
+    results_page = HomePage(py).visit().search('nissan')
+    results_page.filter_search("SKYLINE")
+    try:
+        assert py.contains('SKYLINE22')
+    except:
+        "Could not find element with the text ``SKYLINE22``"
     py.screenshot('../screenshots/test_challenge6_model_not_found.png')
