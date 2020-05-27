49b03bc084bda8ca643ec60f3b5f41d0b03bf939
DaniilVolg <59584072+DaniilVolg@users.noreply.github.com>
2020-05-27 22:50:33 +0300

    Update cert test_challenge3.py

--- cert test_challenge3.py
+++ cert test_challenge3.py
@@ -1,11 +1,6 @@
-def test_challenge31_copart_honda_search(py):
+def test_challenge3(py):
     py.visit('https://copart.com')
-    py.get('[data-uname="homePageFindAVehicle"]').click()
-    py.get('[data-uname="vehicleFinderTab"]').click()
-    py.get("[data-uname=\"vehiclefinderTypedropdownbox\"]").select('Automobile')
-    py.get('[data-uname="vehiclefinderMakedownbox"]').select('Honda')
-    py.get('[data-uname="vehiclefinderModeldownbox"]').select('CRV')
-    py.get('[data-uname="vehiclefinderLocationdropdownbox"]').select('NY - Long Island')
-    py.get('[ng-click="search(form)"]').click()
-    assert py.should().contain_title('Honda')
-    py.get('[name="serverSideDataTable_length"]').select('100').click()
+    popular_models = py.find("li[ng-repeat*='popularSearch']>a")
+    for model in popular_models:
+        print(f'{model.text()} - {model.get_attribute("href")}')
+    assert popular_models.should().have_length(20)
